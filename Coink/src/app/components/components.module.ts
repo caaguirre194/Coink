ea0db9b18a1d6bcb28520866d8b329a650b47c90
Carlos Andres Aguirre Canas <carlosaguirre@coinks-iMac.local>
2020-09-17 15:03:28 -0500

    Se crea primera versión de home page y feed component

--- Coink/src/app/components/components.module.ts
+++ Coink/src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { IonicModule } from "@ionic/angular";
 import { InstructionsComponent } from "./instructions/instructions.component";
+import { FeedComponent } from "./feed/feed.component";
 
 @NgModule({
-  declarations: [HeaderComponent, InstructionsComponent],
+  declarations: [HeaderComponent, InstructionsComponent, FeedComponent],
   imports: [CommonModule, IonicModule],
-  exports: [HeaderComponent, InstructionsComponent],
+  exports: [HeaderComponent, InstructionsComponent, FeedComponent],
 })
 export class ComponentsModule {}
--- Coink/src/app/pages/home/home.page.ts
+++ Coink/src/app/pages/home/home.page.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"],
 })
 export class HomePage implements OnInit {
+  private balances = {
+    disponible: "533.333",
+    total: "1.171.000",
+  };
 
-  constructor() { }
+  private isDay: boolean;
 
-  ngOnInit() {
+  photo = "assets/img/home/main/fondos/Fondo oscuro.png";
+
+  photo2 = `url( ${this.photo})`;
+
+  slideOpts = {
+    // slidesPerView: 10,
+    // freeMode: false,
+    // coverflowEffect: {
+    //   rotate: 50,
+    //   stretch: 0,
+    //   depth: 100,
+    //   modifier: 1,
+    //   slideShadows: true,
+    // },
+  };
+
+  options = [
+    {
+      url: "assets/img/home/options/Cofre cerrado.svg",
+      name: "Comunidades",
+    },
+    {
+      url: "assets/img/home/options/icono_meta.svg",
+      name: "Metas",
+    },
+    {
+      url: "assets/img/home/options/Cofre cerrado.svg",
+      name: "Comunidades",
+    },
+    {
+      url: "assets/img/home/options/icono_meta.svg",
+      name: "Metas",
+    },
+    {
+      url: "assets/img/home/options/Cofre cerrado.svg",
+      name: "Comunidades",
+    },
+    {
+      url: "assets/img/home/options/icono_meta.svg",
+      name: "Metas",
+    },
+  ];
+
+  constructor() {}
+
+  setBackground() {}
+
+  ngOnInit() {}
+
+  onClick() {}
+
+  infoBalance(test: string) {
+    alert(`Esto es un balance ${test}`);
   }
-
 }
