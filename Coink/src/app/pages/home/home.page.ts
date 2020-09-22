b420babf5de83c2071dc49e67e094ca237df4619
Carlos Andres Aguirre Canas <carlosaguirre@coinks-iMac.local>
2020-09-22 14:00:17 -0500

    Se crear pantallas de primera vez en comunidades, metas, tarjeta y kids, además de crear primera versiíon de opción Metas.

--- Coink/src/app/pages/home/home.page.ts
+++ Coink/src/app/pages/home/home.page.ts
@@ -17,34 +17,8 @@ export class HomePage implements OnInit {
   private pockets: any;
   private goals: any;
   private notifications: any;
-
-  private communities = [
-    {
-      name: "Javeriana",
-      value: "100000",
-      url: "assets/img/home/options/javeriana.png",
-    },
-    {
-      name: "Comfama",
-      value: "205350",
-      url: "assets/img/home/options/comfama.png",
-    },
-    {
-      name: "Subsidios",
-      value: "75000",
-      url: "assets/img/home/options/comfama.png",
-    },
-    {
-      name: "Comfama",
-      value: "205350",
-      url: "assets/img/home/options/comfama.png",
-    },
-    {
-      name: "Subsidios",
-      value: "75000",
-      url: "assets/img/home/options/comfama.png",
-    },
-  ];
+  private communities: any;
+  private kids: any;
 
   private bazarItems = [
     {
@@ -88,23 +62,6 @@ export class HomePage implements OnInit {
     {
       url: "assets/img/home/options/Cofre cerrado.svg",
       name: "Comunidades",
-      list: [
-        {
-          icon: "",
-          name: "Javeriana",
-          value: "$100.000",
-        },
-        {
-          icon: "",
-          name: "Comfama",
-          value: "$205.350",
-        },
-        {
-          icon: "",
-          name: "Subsidios",
-          value: "$75.000",
-        },
-      ],
     },
     {
       url: "assets/img/home/options/icono_meta.svg",
@@ -121,10 +78,6 @@ export class HomePage implements OnInit {
     {
       url: "assets/img/home/options/Cofre cerrado.svg",
       name: "Kids",
-    },
-    {
-      url: "assets/img/home/options/Cofre cerrado.svg",
-      name: "Grupos de ahorros",
     },
   ];
 
@@ -195,7 +148,12 @@ export class HomePage implements OnInit {
     this.isDay = !this.isDay;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getCommunities();
+    this.getGoals();
+    this.getPockets();
+    this.getKids();
+  }
 
   onClick() {}
 
@@ -247,6 +205,144 @@ export class HomePage implements OnInit {
         status: 200,
         time_elapsed: 37,
       },
+    ];
+  }
+
+  getCommunities() {
+    this.communities = [
+      {
+        name: "Javeriana",
+        value: "100000",
+        url: "assets/img/home/options/javeriana.png",
+      },
+      {
+        name: "Comfama",
+        value: "205350",
+        url: "assets/img/home/options/comfama.png",
+      },
+      {
+        name: "Subsidios",
+        value: "75000",
+        url: "assets/img/home/options/comfama.png",
+      },
+      {
+        name: "Comfama",
+        value: "205350",
+        url: "assets/img/home/options/comfama.png",
+      },
+      {
+        name: "Subsidios",
+        value: "75000",
+        url: "assets/img/home/options/comfama.png",
+      },
+    ];
+  }
+
+  getKids() {
+    this.kids = [
+      // {
+      //   authorization_id: 1,
+      //   balance: 200000,
+      //   balance_total: 200000,
+      //   count_goals: 3,
+      //   first_color: "#0f591c",
+      //   first_pocket: true,
+      //   general_description:
+      //     "Con el Cofre Comfenalco Valle tienes acceso a grandes beneficios, premios, promociones y mucho más.",
+      //   goal_balance: 0,
+      //   id_aliado: "CORRESP AV DORADO",
+      //   internal_description: "Acceda a grandes beneficios",
+      //   logo:
+      //     "https://s3.amazonaws.com/dev.cdn.bancoink.biz/pockets/2ba6f0886a072d8a21f0d1b9342c0be4.png",
+      //   main_features:
+      //     "Las principales características de este Cofre incluyen↵↵1. Poder ahorrar por separado de tu saldo Coink.↵2. Poder tener un historia…",
+      //   market_place: true,
+      //   name_vault: "DEV - Comfenalco Valle",
+      //   nit_vault: "900300400",
+      //   restrictive: false,
+      //   second_color: "#ffffff",
+      //   type_vault: 3,
+      //   update_term_conditions: true,
+      //   vault_id: "03e9fcd8-63d9-49bc-aaf9-27ac4573d790",
+      // },
+      // {
+      //   authorization_id: 1,
+      //   balance: 0,
+      //   balance_total: 0,
+      //   count_goals: 0,
+      //   first_color: "#07EE1E",
+      //   first_pocket: false,
+      //   general_description:
+      //     "Este es un Cofre personal tuyo. Tú decides cómo usarlo para organizar mejor tus ahorros.",
+      //   goal_balance: 0,
+      //   id_aliado: "",
+      //   internal_description:
+      //     "Este es un Cofre personal tuyo. Tú decides cómo usarlo para organizar mejor tus ahorros.",
+      //   logo:
+      //     "https://s3.amazonaws.com/dev.cdn.bancoink.biz/pockets/5bc8c567a89112d5f408a8af4f17970d.png",
+      //   main_features:
+      //     "Creamos este tipo de Cofre para que tú tengas total control. Acá puedes separar parte de tu saldo, ver un historial específicamente par…",
+      //   market_place: false,
+      //   name_vault: "Prueba",
+      //   nit_vault: "",
+      //   restrictive: false,
+      //   second_color: "#05410B",
+      //   type_vault: 4,
+      //   update_term_conditions: true,
+      //   vault_id: "c1cf9b95-2b06-4d0c-9a83-472cf17e1175",
+      // },
+      // {
+      //   authorization_id: 1,
+      //   balance: 180000,
+      //   balance_total: 180000,
+      //   count_goals: 0,
+      //   first_color: "#07EE1E",
+      //   first_pocket: false,
+      //   general_description:
+      //     "Este es un Cofre personal tuyo. Tú decides cómo usarlo para organizar mejor tus ahorros.",
+      //   goal_balance: 0,
+      //   id_aliado: "",
+      //   internal_description:
+      //     "Este es un Cofre personal tuyo. Tú decides cómo usarlo para organizar mejor tus ahorros.",
+      //   logo:
+      //     "https://s3.amazonaws.com/dev.cdn.bancoink.biz/pockets/99e3c77bd9b8b06a5cbec5ca7ed00a7f.png",
+      //   main_features:
+      //     "Creamos este tipo de Cofre para que tú tengas total control. Acá puedes separar parte de tu saldo, ver un historial específicamente par…",
+      //   market_place: false,
+      //   name_vault: "Viernes",
+      //   nit_vault: "",
+      //   restrictive: false,
+      //   second_color: "#05410B",
+      //   type_vault: 4,
+      //   update_term_conditions: true,
+      //   vault_id: "e2fe5083-4f4e-44ba-9ea6-055e1b525378",
+      // },
+      // {
+      //   authorization_id: 1,
+      //   balance: 0,
+      //   balance_total: 0,
+      //   count_goals: 0,
+      //   first_color: "#07EE1E",
+      //   first_pocket: false,
+      //   general_description:
+      //     "Este es un Cofre personal tuyo. Tú decides cómo usarlo para organizar mejor tus ahorros.",
+      //   goal_balance: 0,
+      //   id_aliado: "",
+      //   internal_description:
+      //     "Este es un Cofre personal tuyo. Tú decides cómo usarlo para organizar mejor tus ahorros.",
+      //   logo:
+      //     "https://s3.amazonaws.com/dev.cdn.bancoink.biz/pockets/faeb94e6b07d2b9e70ee62e17946a8a5.png",
+      //   main_features:
+      //     "Creamos este tipo de Cofre para que tú tengas total control. Acá puedes separar parte de tu saldo, ver un historial específicamente par…",
+      //   market_place: false,
+      //   name_vault: "Hiiy",
+      //   nit_vault: "",
+      //   restrictive: false,
+      //   second_color: "#05410B",
+      //   type_vault: 4,
+      //   update_term_conditions: true,
+      //   vault_id: "9f973926-f265-45dc-889b-a5eb21a71b0c",
+      // },
     ];
   }
 
@@ -367,9 +463,7 @@ export class HomePage implements OnInit {
     const className = "option-selected";
     console.log(this.optionSelected);
 
-    if (event.currentTarget.classList.contains(className)) {
-      event.currentTarget.classList.remove(className);
-    } else {
+    if (!event.currentTarget.classList.contains(className)) {
       const elems = document.querySelectorAll(`.${className}`);
 
       [].forEach.call(elems, (el) => {
