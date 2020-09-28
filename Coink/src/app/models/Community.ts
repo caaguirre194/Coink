e3f31a34ddc67d7f2ab1c3d048565389b36e65d0
Carlos Andres Aguirre Canas <carlosaguirre@coinks-iMac.local>
2020-09-28 12:13:15 -0500

    Se crea segunda versión de Community interface

--- Coink/src/app/models/Community.ts
+++ Coink/src/app/models/Community.ts
@@ -2,7 +2,23 @@
  * User community
  */
 export interface Community {
-  name: string;
-  value: string;
-  url: string;
+  logo: string;
+  name_vault: string;
+  id_aliado: string;
+  nit_vault: string;
+  first_color: string;
+  second_color: string;
+  type_vault: number;
+  general_description: string;
+  internal_description: string;
+  authorization_id: number;
+  main_features: string;
+  restrictive: boolean;
+  balance: number;
+  vault_id: string;
+  update_term_conditions: boolean;
+  count_goals: number;
+  goal_balance: number;
+  market_place: boolean;
+  balance_total: number;
 }
